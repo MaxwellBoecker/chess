10b2577aa9ed32189cfd5111c88e546702728a3c
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-11-06 15:02:59 -0600

    (feat) added the splitSolution function to this file

--- src/puzzleFunctions.js
+++ src/puzzleFunctions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 const findOrientation = (fen) => {
   if (fen.split(' ')[1] === 'b') {
     return 'black';
@@ -5,6 +6,24 @@ const findOrientation = (fen) => {
   return 'white';
 };
 
+const splitSolution = (solution) => {
+  const first = solution[0].slice(1, 3);
+  const second = solution[0].slice(4, 6);
+  let solutionMove;
+  const piece = solution[0][0];
+  if (solution[0][solution[0].length - 1] === '+') {
+    solutionMove = `${second}+`;
+  } else {
+    solutionMove = `${second}`;
+  }
+  let newSolution;
+  solution[0].includes('x')
+    ? newSolution = [`${piece}x${solutionMove}`, ...solution.slice(1)]
+    : newSolution = [`${piece}${solutionMove}`, ...solution.slice(1)];
+  return { first, second, newSolution };
+};
+
 module.exports = {
   findOrientation,
+  splitSolution,
 };
