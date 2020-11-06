0d6bccbb198bd89a290b3b85c48a06fa4007f7df
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-11-06 15:01:46 -0600

    (cleanup) moved logic of splitting the solution for computer move to puzzleFunctions.js

--- src/PuzzleLogic.jsx
+++ src/PuzzleLogic.jsx
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import Chess from 'chess.js'; // import Chess from  "chess.js"(default) if recieving an error about new Chess() not being a constructor
-import { findOrientation } from './puzzleFunctions';
+import { findOrientation, splitSolution } from './puzzleFunctions';
 
 class HumanVsHuman extends Component {
   // static propTypes = { children: PropTypes.func };
@@ -139,21 +139,7 @@ class HumanVsHuman extends Component {
       }, () => {
         const { solution } = this.state;
         if (solution[0] && solution[0].length > 4) {
-          const first = solution[0].slice(1, 3);
-          const second = solution[0].slice(4, 6);
-          let solutionMove;
-          const piece = solution[0][0];
-          if (solution[0][solution[0].length - 1] === '+') {
-            solutionMove = `${second}+`;
-          } else {
-            solutionMove = `${second}`;
-          }
-          let newSolution;
-          solution[0].includes('x')
-            ? newSolution = [`${piece}x${solutionMove}`, ...solution.slice(1)]
-            : newSolution = [`${piece}${solutionMove}`, ...solution.slice(1)];
-
-          // console.log(first, second);
+          const { first, second, newSolution } = splitSolution(solution);
           this.setState({
             solution: newSolution,
           }, () => {
