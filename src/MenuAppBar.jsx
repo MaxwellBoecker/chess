35144aa54f074acb54e9ded8be2dca890d6c3ea3
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-11-30 18:19:32 -0600

    (feat) created Toolbar

---
+++ src/MenuAppBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+function MenuAppBar() {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default MenuAppBar;
