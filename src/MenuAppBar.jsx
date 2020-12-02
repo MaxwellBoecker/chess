387cc04c87746983c05e080d3581a3cccb347073
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-01 19:56:51 -0600

    (feat) added grid layout for MovesBox

--- src/MenuAppBar.jsx
+++ src/MenuAppBar.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { AppBar, Button, Toolbar } from '@material-ui/core';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
 
 function MenuAppBar() {
   return (
     <div>
-      <Router>
-
-        <AppBar>
-          <Toolbar style={{ backgroundColor: '#373737' }}>
-            <Button style={{ color: 'goldenrod' }}>
-              Puzzles
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Router>
+      <AppBar>
+        <Toolbar style={{ backgroundColor: '#373737' }}>
+          <Button style={{ color: 'goldenrod' }}>
+            Puzzles
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
