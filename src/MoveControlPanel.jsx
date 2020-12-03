164f63f7b3792a209a15f7c95bcbe0f2cb28942d
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-02 18:17:25 -0600

    (feat) added MoveControlPanel

---
+++ src/MoveControlPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  BottomNavigation,
+} from '@material-ui/core';
+
+const MoveControlPanel = () => {
+  return(
+    <BottomNavigation style={{ backgroundColor: '#252525'}}></BottomNavigation>
+  );
+};
+
+export default MoveControlPanel;
