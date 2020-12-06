9148f2888d3e8254e68bb09dd956c106285cec6f
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-06 15:38:10 -0600

    (feat) Stopwatch runs correctly, starts and stops correctly

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   BottomNavigation,
   Button,
@@ -7,55 +7,42 @@ import {
 const Stopwatch = () => {
   const [timerOn, setTimerOn] = useState(false);
   const [timerTime, setTimerTime] = useState(0);
-  const [timerStart, setTimerStart] = useState(0);
-  const onClick = (e) => {
-    setTimerOn(!timerOn);
-    console.log(timerOn);
+  let timerStart = 0;
+  const [timer, setTimer] = useState(null);
+
+  const timerSetup = () => setInterval(() => {
+    setTimerTime(Date.now() - timerStart);
+  }, 1000);
+  useEffect(() => {
+    timerStart = Date.now();
+  });
+  const onClick = () => {
+    if (timerOn === false) {
+      setTimer(timerSetup());
+      setTimerOn(true);
+    } else {
+      clearInterval(timer);
+      setTimerOn(false);
+    }
   };
+
+  const seconds = (`0${Math.floor(timerTime / 1000) % 60}`).slice(-2);
+  const minutes = (`0${Math.floor(timerTime / 60000) % 60}`).slice(-2);
   return (
     <div style={{ backgroundColor: 'goldenrod', color: 'white' }}>
-        <Button onClick={onClick} style={{ color: 'black' }}>
-          start/stop
-        </Button>
+      <div className="Stopwatch-display" style={{ color: 'white' }}>
+
+        {minutes}
+        {' '}
+        :
+        {seconds}
+      </div>
+      <Button onClick={onClick} style={{ color: 'black' }}>
+        start/stop
+      </Button>
     </div>
   );
 };
 
-
 export default Stopwatch;
-// const Stopwatch = () => {
-//   const [timerOn, setTimerOn] = useState(false);
-//   const [timerTime, setTimerTime] = useState(0);
-//   const [timerStart, setTimerStart] = useState(0);
-//   const timer = setInterval(() => {
-//     setTimerTime(Date.now() - timerStart);
-//   }, 100);
 
-//   const startTimer = () => {
-//     setTimerOn(true);
-//     setTimerTime(timerTime);
-//     setTimerStart(Date.now() - timerTime);
-//     //timer();
-//   };
-
-//   const stopTimer = () => {
-//     setTimerOn(false);
-//     clearInterval(timer);
-//   };
-//   const seconds = (`0${Math.floor(timerTime / 1000) % 60}`).slice(-2);
-//   const minutes = (`0${Math.floor(timerTime / 60000) % 60}`).slice(-2);
-//   return (
-//     <>
-//     <div className="Stopwatch-display" style={{ color: 'white' }}>
-
-//       {minutes}
-//       {' '}
-//       :
-//       {seconds}
-//     </div>
-
-//     </>
-//   );
-// };
-
-// export default Stopwatch;
