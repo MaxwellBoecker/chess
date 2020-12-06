88f00727c40847f8591766ac4497699b97f4bf22
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-06 16:20:01 -0600

    (cleanup) took out button

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
-  BottomNavigation,
-  Button,
+  Container,
 } from '@material-ui/core';
 
 const Stopwatch = (props) => {
@@ -26,11 +25,11 @@ const Stopwatch = (props) => {
     }
   };
   if (stopClock === false && count === 0) {
-    console.log('in here')
+    console.log('in here');
     timerStart = Date.now();
     onClick();
     setCount(1);
-  } else if(stopClock === true && count === 1){
+  } else if (stopClock === true && count === 1) {
     onClick();
     setCount(2);
   }
@@ -38,7 +37,7 @@ const Stopwatch = (props) => {
   const seconds = (`0${Math.floor(timerTime / 1000) % 60}`).slice(-2);
   const minutes = (`0${Math.floor(timerTime / 60000) % 60}`).slice(-2);
   return (
-    <div style={{ backgroundColor: 'goldenrod', color: 'white' }}>
+    <Container style={{ backgroundColor: 'goldenrod', color: 'white' }}>
       <div className="Stopwatch-display" style={{ color: 'white' }}>
 
         {minutes}
@@ -46,10 +45,7 @@ const Stopwatch = (props) => {
         :
         {seconds}
       </div>
-      <Button onClick={onClick} style={{ color: 'black' }}>
-        start/stop
-      </Button>
-    </div>
+    </Container>
   );
 };
 
