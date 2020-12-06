235f9d1a019dd13e4efe8314b8492b981e15874f
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-06 16:17:11 -0600

    (feat) stopwatch auto-starts and stops on success or failure

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -4,18 +4,18 @@ import {
   Button,
 } from '@material-ui/core';
 
-const Stopwatch = () => {
+const Stopwatch = (props) => {
+  const { stopClock } = props;
   const [timerOn, setTimerOn] = useState(false);
   const [timerTime, setTimerTime] = useState(0);
   let timerStart = 0;
   const [timer, setTimer] = useState(null);
+  const [count, setCount] = useState(0);
 
   const timerSetup = () => setInterval(() => {
     setTimerTime(Date.now() - timerStart);
   }, 1000);
-  useEffect(() => {
-    timerStart = Date.now();
-  });
+
   const onClick = () => {
     if (timerOn === false) {
       setTimer(timerSetup());
@@ -25,6 +25,15 @@ const Stopwatch = () => {
       setTimerOn(false);
     }
   };
+  if (stopClock === false && count === 0) {
+    console.log('in here')
+    timerStart = Date.now();
+    onClick();
+    setCount(1);
+  } else if(stopClock === true && count === 1){
+    onClick();
+    setCount(2);
+  }
 
   const seconds = (`0${Math.floor(timerTime / 1000) % 60}`).slice(-2);
   const minutes = (`0${Math.floor(timerTime / 60000) % 60}`).slice(-2);
@@ -45,4 +54,3 @@ const Stopwatch = () => {
 };
 
 export default Stopwatch;
-
