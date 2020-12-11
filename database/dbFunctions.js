b463ca9904c63227ae88181e3923a65dd292516e
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-11 16:08:20 -0600

    (feat) added DB functions

--- database/dbFunctions.js
+++ database/dbFunctions.js
@@ -5,9 +5,32 @@ const { DATABASE } = process.env;
 
 async function createPuzzle(client, newPuzzle) {
   const result = await client.db(DATABASE).collection('puzzles').insertOne(newPuzzle);
-  console.log(`new document created`);
+  console.log('new document created');
+}
+
+async function connectAndRetrievePuzzle(client, options) {
+  let result;
+  try {
+    await client.connect();
+    result = await client.db(DATABASE).collection('puzzles').findOne(options);
+  } catch (e) {
+    console.log(e);
+  }
+  return result;
+}
+
+async function connectAndInsertPuzzle(client, puzzleInfo) {
+  try {
+    await client.connect();
+    console.log('database connected');
+    await createPuzzle(client, puzzleInfo);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = {
   createPuzzle,
+  connectAndRetrievePuzzle,
+  connectAndInsertPuzzle,
 };
