06a50c101a93313caaddea4df034a8b71503627f
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-12-15 15:58:55 -0600

    (cleanup) commented out unused functions

--- database/index.js
+++ database/index.js
@@ -4,40 +4,31 @@ const { MongoClient } = require('mongodb');
 dotenv.config();
 
 const { MONGO_CONNECTION_STRING } = process.env;
-const client = new MongoClient(MONGO_CONNECTION_STRING, { useUnifiedTopology: true });
 
-async function listDatabases(client) {
-  const databasesList = await client.db().admin().listDatabases();
+const createNewClient = () => new MongoClient(MONGO_CONNECTION_STRING, { useUnifiedTopology: true });
 
-  console.log('Databases:');
-  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
-}
+// async function listDatabases(client) {
+//   const databasesList = await client.db().admin().listDatabases();
 
-async function connectToDB() {
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
+//   console.log('Databases:');
+//   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+// }
 
-
-  try {
-    // Connect to the MongoDB cluster
-    await client.connect();
-    console.log('database connected');
-    await listDatabases(client);
-    // Make the appropriate DB calls
-    
-  } catch (e) {
-    console.error(e);
-  
-  } finally {
-    await client.close();
-    console.log('closed');
-  }
-}
+// async function connectToDB() {
+//   try {
+//     // Connect to the MongoDB cluster
+//     await client.connect();
+//     console.log('database connected');
+//     await listDatabases(client);
+//     // Make the appropriate DB calls
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//     console.log('closed');
+//   }
+// }
 module.exports = {
-  connectToDB,
-  listDatabases,
-  client,
+  createNewClient,
 };
 // main().catch(console.error);
